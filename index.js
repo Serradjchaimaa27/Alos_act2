67bc4faff7c9b9a7899a807044c98417b7d7e6a1
Serradjchaimaa27 <101481298+Serradjchaimaa27@users.noreply.github.com>
2022-04-01 18:20:35 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const app = express();
+const surahs = require('./surahs.json');
+const { body, validationResult } = require('express-validator'); 
 
+
+app.use(express.json());
+app.use(logErrors);
+app.use(surahsErrorHandler);
+app.use(errorHandler);
